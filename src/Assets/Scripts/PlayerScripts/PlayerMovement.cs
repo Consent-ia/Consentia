3ead4d4fe2300ad4105b42e4199abcbd055d7b0e
NgYaoDong <e0957740@u.nus.edu>
2025-10-26 17:17:01 +0800

    Update Unity version, and add PlayerMovement Script

---
+++ src/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5.0f;
+
+    private Rigidbody2D rigidBody;
+    private Animator animator;
+    private Collider2D colli;
+
+    Vector2 movement;
+    private Camera PlayerCamera;
+
+    public Camera GetPlayerCamera()
+    {
+        return PlayerCamera;
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        colli = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.MovePosition(rigidBody.position + movement.normalized * speed * Time.fixedDeltaTime);
+    }
+}
