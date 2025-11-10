7eb078fa1ac7902e64c8634f0480679b9abd9611
NgYaoDong <e0957740@u.nus.edu>
2025-11-10 16:06:44 +0800

    feat: - Add tilemap for background - Add tilemap for SolidObjects (walls, trees, rocks, etc) - Setup Colliders for SolidObjects

--- src/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ src/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -9,15 +9,13 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D rigidBody;
     private Animator animator;
-    private Collider2D colli;
 
     Vector2 movement;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-        colli = GetComponent<Collider2D>();
+        //animator = GetComponent<Animator>();
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -28,9 +26,9 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
+        //animator.SetFloat("Horizontal", movement.x);
+        //animator.SetFloat("Vertical", movement.y);
+        //animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     private void FixedUpdate()
