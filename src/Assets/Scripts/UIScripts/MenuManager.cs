74fcc79f35f225feb7a969d6c821fbf1c86e88a7
NgYaoDong <e0957740@u.nus.edu>
2026-01-28 17:21:54 +0800

    feat: Add Menu functionality - Use Tab button to activate Menu for settings and to pause the game

---
+++ src/Assets/Scripts/UIScripts/MenuManager.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mainMenu;
+
+    void Start()
+    {
+        CloseMenu();
+    }
+
+    public void Menu(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (mainMenu.activeSelf)
+            {
+                CloseMenu();
+                Debug.Log("Closing Menu");
+            }
+            else
+            {
+                OpenMenu();
+                Debug.Log("Opening Menu");
+            }
+        }
+    }
+
+    private void CloseMenu()
+    {
+        mainMenu.SetActive(false);
+        Time.timeScale = 1f; // Resume the game
+    }
+
+    private void OpenMenu()
+    {
+        mainMenu.SetActive(true);
+        Time.timeScale = 0f; // Pause the game
+    }
+
+    void Update()
+    {
+        
+    }
+}
